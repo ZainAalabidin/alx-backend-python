f6b60120286219df6c2a0c5c294afa2e78969eea
ZainAalabidin <zynly9303@gmail.com>
2024-08-24 06:24:54 +0000

    ok

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+ascript for a type-annotated function add that takes a float a and a float b 
+as arguments and returns their sum as a float.
+"""
+
+def add(a: float , b: float) -> float:
+    """
+    args:
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+        float: _description_
+
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+type annotatasions folder
