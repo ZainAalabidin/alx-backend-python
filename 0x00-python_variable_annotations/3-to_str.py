128ce42e89eda6537756ad03ff25097ca10682b4
ZainAalabidin <zynly9303@gmail.com>
2024-08-24 07:37:00 +0000

    ok

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+script for a type-annotated function to_str that takes a float n as argument
+and returns the string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """_summary_
+
+    args:
+        n (float): float
+
+    Returns: srting
+    """
+
+    return str(n)
