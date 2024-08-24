9e99b2199005796ba31e837c839351984a2f0859
ZainAalabidin <zynly9303@gmail.com>
2024-08-24 06:51:19 +0000

    ok

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+script for type-annotated function concat that takes a string str1 and a string
+str2 as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    args:
+        str1 (string): string
+        str1 (string): string
+    Returns:
+        string: a concatinating string
+
+    """
+    return str1+str2
